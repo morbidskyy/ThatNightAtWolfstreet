92589dd845875cef361a78f46b9b87428f53235c
B1b1b0 <gprzybyek@gmail.com>
2026-01-22 21:23:49 +0100

    updated the resume screen

--- scripts/options.gd
+++ scripts/options.gd
@@ -7,4 +7,4 @@ func _on_sound_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_options_return_pressed() -> void:
-	pass # Replace with function body.
+	pass
---
+++ scripts/pause.gd
@@ -0,0 +1,33 @@
+extends Control
+
+func _on_ready() -> void:
+	$AnimationPlayer.play("RESET")
+	hide()
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+	hide()
+	
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+	show()
+
+func test_esc():
+	if Input.is_action_just_pressed("ui_cancel") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("ui_cancel") and get_tree().paused == true:
+		resume()
+
+func _process(delta: float) -> void:
+	test_esc()
+
+func _on_resume_pressed() -> void:
+	resume()
+
+func _on_pause_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
+
+func _on_start_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
