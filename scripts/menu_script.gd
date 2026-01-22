27a51a0d59ccfa29be7d26069514d37d8dee93a1
Croixsant <149088478+Croixsant@users.noreply.github.com>
2026-01-22 19:35:39 +0100

    jestem debilem pomyliłam menu i main

--- scripts/menu_script.gd
+++ scripts/menu_script.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
 func _on_options_button_pressed() -> void:
