a6471620e714bb08def709faec0e2241586c7f8e
B1b1b0 <gprzybyek@gmail.com>
2026-01-22 19:49:06 +0100

    added option and pause menu

---
+++ scripts/guziki_opcje.gd
@@ -0,0 +1,10 @@
+extends VBoxContainer
+
+func _on_język_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_dźwięk_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_powrót_opcje_pressed() -> void:
+	pass # Replace with function body.
