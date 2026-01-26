845f58e3cbe4f58390bf49dff01ef117ce6a29fb
B1b1b0 <gprzybyek@gmail.com>
2026-01-26 01:05:47 +0100

    removed shortcut to options from pause menu and added the tileset

--- scripts/pause.gd
+++ scripts/pause.gd
@@ -25,8 +25,5 @@ func _process(delta: float) -> void:
 func _on_resume_pressed() -> void:
 	resume()
 
-func _on_pause_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/options.tscn")
-
 func _on_start_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
