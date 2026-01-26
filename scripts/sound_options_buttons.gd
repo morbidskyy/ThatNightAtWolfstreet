e0f90f741a762bd06e78e1f8f0e707a23a2700a1
B1b1b0 <gprzybyek@gmail.com>
2026-01-26 21:43:38 +0100

    scripted volume sliders (yet to test)

--- scripts/sound_options_buttons.gd
+++ scripts/sound_options_buttons.gd
@@ -1,10 +1,4 @@
 extends VBoxContainer
 
-func _on_english_pressed() -> void:
-	pass
-
-func _on_polish_pressed() -> void:
-	pass # Replace with function body.
-
 func _on_language_options_return_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options.tscn")
