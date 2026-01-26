b46a58610f225bd9c77b82bad1b9380902b17a76
morbidskyy <mazurekiga23@gmail.com>
2026-01-26 20:21:59 +0100

    p3 animations added

---
+++ scripts/player_3.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@onready var animation_p3: AnimationPlayer = $player3_sprite/animation_p3
+
+var speed : int = 250
+
+
+func get_input():
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_dir * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_collide(velocity * delta)
+
+@warning_ignore("unused_parameter")
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_up"):
+		animation_p3.play("p3_back_walk")
+	elif Input.is_action_pressed("ui_down"):
+		animation_p3.play("p3_front_walk")
+	elif Input.is_action_pressed("ui_right"):
+		animation_p3.play("p3_right_walk")
+	elif Input.is_action_pressed("ui_left"):
+		animation_p3.play("p3_left_walk")
+	else:
+		animation_p3.play("p3_idle")
--- scripts/top_down_controls.gd
+++ scripts/top_down_controls.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 var speed : int = 250
 
+
 func get_input():
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_dir * speed
