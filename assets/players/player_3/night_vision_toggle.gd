c838ceace4179945c76e2993877af25814f611c3
morbidskyy <mazurekiga23@gmail.com>
2026-01-27 16:59:22 +0100

    fixed night vision
    it actually works now

--- assets/players/player_3/night_vision_toggle.gd
+++ assets/players/player_3/night_vision_toggle.gd
@@ -4,7 +4,7 @@ func _on_ready() -> void:
 	$nv_animation.play("RESET")
 	hide()
 	
-func _input(event):
-	if event.is_action_pressed("toggle_nv"):
+func _input(_event):
+	if Input.is_action_just_pressed("toggle_nv"):
 		$nv_animation.play("nv_animation")
-		visible = !visible
+		show()
