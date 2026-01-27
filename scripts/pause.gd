9c44defd027efa284e5fdbdc1b0ad4014af62000
B1b1b0 <gprzybyek@gmail.com>
2026-01-27 21:28:33 +0100

    coded player 2, added the second tileset

--- scripts/pause.gd
+++ scripts/pause.gd
@@ -19,7 +19,7 @@ func test_esc():
 	elif Input.is_action_just_pressed("ui_cancel") and get_tree().paused == true:
 		resume()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	test_esc()
 
 func _on_resume_pressed() -> void:
