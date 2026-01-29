a8f704e597fd651f2a64e9ef95069ecb95714d94
Croixsant <149088478+Croixsant@users.noreply.github.com>
2026-01-29 20:27:20 +0100

    Menu work

--- scripts/options.gd
+++ scripts/options.gd
@@ -7,4 +7,4 @@ func _on_sound_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/sound_options.tscn")
 
 func _on_options_return_pressed() -> void:
-	pass
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
