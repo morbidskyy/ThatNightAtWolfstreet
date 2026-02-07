045584a0a4b547028fc9fd90db67706cdbe37c33
morbidskyy <149096814+morbidskyy@users.noreply.github.com>
2026-02-07 21:57:39 +0100

    fixed nv cooldowns (not fully)

--- scripts/canvas_dark.gd
+++ scripts/canvas_dark.gd
@@ -5,13 +5,15 @@ var is_ready = true
 func _on_ready() -> void:
 	show()
 
-func _input(event):
-	if event.is_action_pressed("toggle_nv"):
-		is_ready = false
-		$"../nv_cooldown".start()
+func _input(_event):
+	if Input.is_action_just_pressed("toggle_nv") and is_ready:
 		hide()
+		is_ready = false
+		$"../nv_duration".start()
 
-
-func _on_nv_cooldown_timeout():
-	is_ready = true
+func _on_nv_duration_timeout() -> void:
 	show()
+	$"../nv_cooldown".start()
+
+func _on_nv_cooldown_timeout() -> void:
+	is_ready = true
