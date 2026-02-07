9971bdb6b883e13e41cca2318b0e16d79cb6eeb7
B1b1b0 <gprzybyek@gmail.com>
2026-02-07 22:28:33 +0100

    changed the options menu - deleted the separate sound option scene and moved the volume slider to general options

--- scripts/options.gd
+++ scripts/options.gd
@@ -3,8 +3,8 @@ extends VBoxContainer
 func _on_language_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/language_options.tscn")
 
-func _on_sound_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/sound_options.tscn")
-
 func _on_options_return_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+func _on_master_value_changed(value: float) -> void:
+	pass # Replace with function body.
