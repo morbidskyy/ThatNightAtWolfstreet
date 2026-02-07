3bb4941e79bc14f781a1fb483c8cedee09bc379f
morbidskyy <149096814+morbidskyy@users.noreply.github.com>
2026-02-07 20:35:33 +0100

    started work on a cooldown for nv_toggle

--- scripts/canvas_dark.gd
+++ scripts/canvas_dark.gd
@@ -1,10 +1,17 @@
 extends CanvasModulate
 
+var is_ready = true
 
-# Called when the node enters the scene tree for the first time.
 func _on_ready() -> void:
 	show()
 
 func _input(event):
 	if event.is_action_pressed("toggle_nv"):
-		visible = !visible
+		is_ready = false
+		$"../nv_cooldown".start()
+		hide()
+
+
+func _on_nv_cooldown_timeout():
+	is_ready = true
+	show()
