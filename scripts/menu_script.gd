c98fb173bbfebafa996a478e8710181d370d2baf
B1b1b0 <gprzybyek@gmail.com>
2026-02-08 18:33:36 +0100

    added tutorial screen (work in progress) and updated manu menu screen

--- scripts/menu_script.gd
+++ scripts/menu_script.gd
@@ -9,5 +9,9 @@ func _on_options_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options.tscn")
 
 
+func _on_tutorial_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/tutorial_screen.tscn")
+
+
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
---
+++ scripts/tutorial_screen.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+
+
+func _on_tutorial_screen_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
