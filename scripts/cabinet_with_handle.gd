799c20337da0d41185b36e60660ef0f1413079a9
B1b1b0 <gprzybyek@gmail.com>
2026-02-11 22:20:00 +0100

    minor changes to lvl 2 layout

--- scripts/cabinet_with_handle.gd
+++ scripts/cabinet_with_handle.gd
@@ -5,10 +5,11 @@ signal door_opened
 var handle_taken = false
 var in_door_zone = false
 
-func _on_area_2d_body_entered(_body: CharacterBody2D) -> void:
-	if handle_taken == false and Input.is_action_just_pressed("interact"):
-		handle_taken = true
-		print("handle picked up")
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	if handle_taken == false:
+		if Input.is_action_just_pressed("interact"):
+			handle_taken = true
+			print("handle picked up")
 
 func _process(_delta: float) -> void:
 	if handle_taken == true:
